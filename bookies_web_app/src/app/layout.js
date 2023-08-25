cb431d43c02dcc6960bc4559263314b236d3a025
ArkaProbhaRoy <arkaarkaroy111probha@gmail.com>
2023-08-26 01:08:05 +0530

    comt nav

--- bookies_web_app/src/app/layout.js
+++ bookies_web_app/src/app/layout.js
@@ -11,9 +11,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Nav/>{children}</body>
-    </html>
+
+    <>
+        <Nav/>
+    <body className={inter.className}>
+        {/* <Nav/> */}
+        {children}
+        </body>
+    </>
   )
 }
