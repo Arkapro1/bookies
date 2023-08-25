370f2c96fb6d700f96c4bdacc78d37562c0c9b8d
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-08-25 09:36:20 +0530

    ghjkl;

---
+++ bookies_web_app/src/app/api/documents/[fid]/route.js
@@ -0,0 +1,40 @@
+
+import Documents from "@/models/documentModel";
+import connect from "@/utils/database";
+import Folders from "@/models/folderModel";
+import { NextResponse } from "next/server";
+// import { useParams } from 'next/navigation';
+
+export const GET=async(request,{params})=>{
+    try {
+        await connect();
+        const fid=params.fid;
+        const folder = await Folders.findById(fid).populate({path:"documents"})
+        return new NextResponse(JSON.stringify(folder.documents),{status:200});
+        // return new NextResponse(fid,{status:200});
+    } catch (error) {
+        return new NextResponse("Error",{status:400});
+    }
+}
+
+export const POST=async(request,{params})=>{
+    const body=await request.json(); 
+    const newDoc=new Documents(body);
+    
+    try {
+        await connect();
+        await newDoc.save();    
+       
+        const fid=params.fid;
+        await Folders.updateOne(
+            { _id: fid},
+            {
+                $push: {
+                    documents: [newDoc._id]
+                }
+            });
+        return new NextResponse("Doc created",{status:200});
+    } catch (error) {
+        return new NextResponse(error,{status:400});
+    }
+}
