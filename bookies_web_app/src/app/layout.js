22be38bfea5412d30e6b60072c2824b2bfcc879e
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-08-27 01:03:26 +0530

    bnm

--- bookies_web_app/src/app/layout.js
+++ bookies_web_app/src/app/layout.js
@@ -12,12 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
+    <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
         <Nav/>{children}
         </AuthProvider>
         </body>
-    </>
+    </html>
   )
 }
