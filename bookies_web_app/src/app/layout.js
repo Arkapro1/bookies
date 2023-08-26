7f7a6d6063daeb188a6a684ba6a7e599ceacca1d
ArkaProbhaRoy <arkaarkaroy111probha@gmail.com>
2023-08-27 01:15:13 +0530

    auth with nav ham* menu

--- bookies_web_app/src/app/layout.js
+++ bookies_web_app/src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
+    <html>
       <body className={inter.className}>
         <AuthProvider>
         <Nav/>{children}
