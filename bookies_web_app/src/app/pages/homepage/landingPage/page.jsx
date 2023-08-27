644847528199b6d13484f481d5b65ecee0008fc2
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-08-27 20:10:33 +0530

    bnm

--- bookies_web_app/src/app/pages/homepage/landingPage/page.jsx
+++ bookies_web_app/src/app/pages/homepage/landingPage/page.jsx
@@ -7,7 +7,7 @@ const Page = (params) => {
             <section class="text-gray-600 body-font">
                 <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                        <span class=" typewriter text-orange-300 text-2xl text   p-5 rounded-3xl terminal">
+                        <span class=" typewriter text-orange-300 text-2xl text   rounded-3xl terminal">
                             <h1>Unleash Smarter Bookmark Management 🔐</h1>
                         </span>
                         <br />
--- bookies_web_app/src/components/nav/nav.jsx
+++ bookies_web_app/src/components/nav/nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
   return (
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div class="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" class="flex items-center">
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Bookie
