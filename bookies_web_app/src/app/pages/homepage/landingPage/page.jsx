c6bc3ec704f9148f9300766559b93fe11796afad
ArkaProbhaRoy <arkaarkaroy111probha@gmail.com>
2023-08-27 13:02:53 +0530

    started

--- bookies_web_app/src/app/pages/homepage/landingPage/page.jsx
+++ bookies_web_app/src/app/pages/homepage/landingPage/page.jsx
@@ -14,14 +14,37 @@ const Page = (params) => {
                         </h1>
                         <br />
 
-                        <div class="mb-8 leading-relaxed text-gradient-purple-green">
+                        {/* <div class="mb-8 leading-relaxed text-gradient-purple-green">
                             <ul class=" list-inside">
                                 <li class="mb-2"><strong class="text-blue-500">Seamless Organization:</strong> Effortlessly categorize and nest bookmarks for quick retrieval.</li>
                                 <li class="mb-2"><strong class="text-blue-500">Universal Access:</strong> Access your bookmarks anytime, anywhere, across all devices.</li>
                                 <li class="mb-2"><strong class="text-blue-500">Intelligent Suggestions:</strong> Discover new content with personalized bookmark recommendations.</li>
                                 <li class="mb-2"><strong class="text-blue-500">Effortless Sharing:</strong> Collaborate by sharing curated folders with friends and colleagues.</li>
                             </ul>
-                        </div>
+                        </div> */}
+                        
+<h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Password requirements:</h2>
+<ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+        At least 10 characters
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+        At least one lowercase character
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 mr-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+        At least one special character, e.g., ! @ # ?
+    </li>
+</ul>
+
 
 
 
