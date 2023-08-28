5fc06d34157c7ceee6158f61313d358594ed668b
Deb-cybe <dmjee1331@gmail.com>
2023-08-28 19:27:07 +0530

    working on user page

--- bookies_web_app/src/components/DocsUnderWorkSpace/page.jsx
+++ bookies_web_app/src/components/DocsUnderWorkSpace/page.jsx
@@ -1,19 +1,194 @@
 "use client";
+import axios from "axios";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 import Workspace from "../workSpaces/page";
+
 const SubFolders = () => {
+  const handleLink = (e) => {
+    e.preventDefault();
+    // alert(12);
+    const val = setVal(prompt("Enter a link"));
+  };
+  const [val, setVal] = useState("def");
+  console.log(val);
   const { data: session, status } = useSession();
   const [toggle, setToggle] = useState(false);
+  const [toggle1, setToggle1] = useState(false);
   if (status == "unauthenticated") {
     redirect("/");
   }
-  const handleFile = (e) => {
-    e.preventDefault();
-    redirect("/pages/upload");
+  //   const handleFile = (e) => {
+  //     e.preventDefault();
+  //     redirect("/pages/upload");
+  //   };
+  let mail = session?.user?.email;
+  console.log(mail);
+  const WorkspacesApi = "/api/folders";
+
+  const [workspaces, setWorkspaces] = useState([]);
+  const [constworkspaces, constsetWorkspaces] = useState([]);
+  const [newWorkspace, setNewWorkspace] = useState({
+    gmail: "gm",
+    name: "",
+    description: "",
+    isWorkSpace: true,
+  });
+  const formselect = ["CREATE", "UPDATE"];
+  const [formtype, setFormtype] = useState(formselect[0]);
+  let [foldereditApi, foldereditApiSet] = useState("");
+
+  const folderEdit = async () => {
+    console.log(foldereditApi);
+    await axios.put(foldereditApi, newWorkspace);
+    await getWorkspaces();
   };
+  if (status == "unauthenticated") {
+    redirect("/");
+  }
+  const updateFormFill = (name, description) => {
+    setNewWorkspace({ ...newWorkspace, name, description });
+  };
+  const updateWorkspace = () => {};
+  const searchResult = (e) => {
+    const filterdata = constworkspaces.filter((ele) => {
+      return ele.name.includes(e.target.value);
+    });
+    setWorkspaces(filterdata);
+  };
+  const getWorkspaces = async () => {
+    const content = await axios.get(WorkspacesApi);
+    setWorkspaces(content.data);
+    constsetWorkspaces(content.data);
+  };
+  const createWorkspace = async () => {
+    await axios.post(WorkspacesApi, newWorkspace);
+    await getWorkspaces();
+  };
+  useEffect(() => {
+    getWorkspaces();
+  }, []);
   return (
     <>
+      {/* Folder Modal */}
+      {toggle1 && (
+        <div className=" fixed z-50 flex w-screen h-screen justify-center items-center bg-zinc-950/70 ">
+          <div
+            id="authentication-modal"
+            tabindex="-1"
+            aria-hidden="true"
+            className="w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          >
+            <div className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto">
+              {/* <!-- Modal content --> */}
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button
+                  onClick={() => {
+                    setToggle1((prev) => !prev);
+                    setNewWorkspace({
+                      name: "",
+                      description: "",
+                      isWorkSpace: true,
+                    });
+                  }}
+                  type="button"
+                  className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="authentication-modal"
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Create Workspace</span>
+                </button>
+                <div className="px-6 py-6 lg:px-8">
+                  <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                    {formtype == "CREATE" ? "Create" : "Update"} New Workspace
+                  </h3>
+                  <form className="space-y-6" action="#">
+                    <div>
+                      <label
+                        for="name"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Workspace Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                        placeholder="Workspace Name"
+                        required
+                        value={newWorkspace.name}
+                        onChange={(e) =>
+                          setNewWorkspace({
+                            ...newWorkspace,
+                            name: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="description"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Description
+                      </label>
+                      <textarea
+                        rows="5"
+                        type="text"
+                        name="description"
+                        id="description"
+                        placeholder="Description"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                        required
+                        value={newWorkspace.description}
+                        onChange={(e) =>
+                          setNewWorkspace({
+                            ...newWorkspace,
+                            description: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+
+                    <button
+                      type="button"
+                      onClick={() => {
+                        formtype == "CREATE" ? createWorkspace() : folderEdit();
+                        setToggle(false);
+                        setNewWorkspace({
+                          name: "",
+                          description: "",
+                          isWorkSpace: true,
+                        });
+                      }}
+                      className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      {formtype == "CREATE" ? "Create" : "Update"}
+                      {/* Create */}
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       {/* <!-- Main modal --> */}
 
       {toggle && (
@@ -74,16 +249,15 @@ const SubFolders = () => {
                         <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
                         <span class="relative">Quick notes</span>
                       </a>
+                      <button onClick={handleLink}>
+                        <a class="ml-10 mb-5 relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group">
+                          <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                          <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                          <span class="relative">Embed Links</span>
+                        </a>
+                      </button>
                       <a
-                        href="#_"
-                        class="ml-10 mb-5 relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
-                      >
-                        <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
-                        <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-                        <span class="relative">Embed Links</span>
-                      </a>
-                      <a
-                        href="#_"
+                        href="/pages/upload"
                         class="ml-10 mb-5 relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
                       >
                         <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-400 rounded-full group-hover:w-56 group-hover:h-56"></span>
@@ -104,12 +278,22 @@ const SubFolders = () => {
             <h2 class="mb-4 inline text-start font-serif text-3xl font-bold text-gray-200 md:mb-6 md:text-4xl">
               Welcome {session?.user.name}
             </h2>
-            <div className="columns-2 ">
+
+            <div className="columns-2  ">
+              <button
+                onClick={() => setToggle1((prev) => !prev)}
+                data-modal-target="authentication-modal"
+                data-modal-toggle="authentication-modal"
+                class="sm:ml-10 mr-3 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-blue-800 "
+                type="button"
+              >
+                Create
+              </button>
               <button
                 onClick={() => setToggle((prev) => !prev)}
                 data-modal-target="authentication-modal"
                 data-modal-toggle="authentication-modal"
-                class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 lg:ml-16 xl:ml-40"
+                class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
                 type="button"
               >
                 Add-on
