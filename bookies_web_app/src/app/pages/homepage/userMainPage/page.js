8b7e633367cc29efd247e641c2080008c8f78f45
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-09-01 12:20:55 +0530

    Update page.js

--- bookies_web_app/src/app/pages/homepage/userMainPage/page.js
+++ bookies_web_app/src/app/pages/homepage/userMainPage/page.js
@@ -274,7 +274,7 @@ const userMainPage=()=>{
         <div className=" mb-4 flex flex-wrap h-10 ">
                 <input
                     type="search"
-                    className=" m-0 w-38 min-w-0 flex-auto rounded-md border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                    className=" m-0 w-38 min-w-0 flex-auto rounded-md border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-white dark:placeholder:text-neutral-200 dark:focus:border-primary"
                     placeholder="Search"
                     aria-label="Search"
                     aria-describedby="button-addon2" onChange={(e)=>searchResult(e)} />
