694aa92971a8c6dc3c01129ed6017cc64c705cc3
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-12-24 16:05:33 +0530

    Terminal size & bgcolor modified

--- bookies_web_app/src/components/terminal/page.jsx
+++ bookies_web_app/src/components/terminal/page.jsx
@@ -1,35 +1,37 @@
-const Terminal=()=>{
-    return(
-        <>
-<div class="w-60% mx-auto">
-  <div class="w-full shadow-2xl subpixel-antialiased rounded h-64 bg-black border-black mx-auto">
-    <div class="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black" id="headerTerminal">
-      <div class="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" id="closebtn">
+const Terminal = () => {
+  return (
+    <>
+      <div class="w-60% mx-auto">
+        {/* Terminal Window  */}
+        <div class="w-full shadow-2xl subpixel-antialiased rounded h-64 bg-gray-800 border-black mx-auto">
+          {/* Terminal Header  */}
+          <div class="flex items-center h-8 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black" id="headerTerminal">
+            <div class="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" id="closebtn">
+            </div>
+            <div class="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3" id="minbtn">
+            </div>
+            <div class="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3" id="maxbtn">
+            </div>
+            <div class="mx-auto pr-16" id="terminaltitle">
+              <p class="text-center text-lg font-semibold">Terminal</p>
+            </div>
+          </div>
+          {/* Terminal console  */}
+          <div class="pl-1 pt-1 h-auto  text-green-200 font-mono text-base bg-gray-800 " id="console">
+            <p class="pb-1">How it helps to boost our Productivity:</p>
+            <h3 class="pb-1">In the realm of productivity, Bookies have emerged as a guiding light
+              for digital nomads. Regardless of the diverse locations they traverse,
+              Bookies empower these modern wanderers to seamlessly manage their
+              bookmarks. With universal accessibility, digital nomads can retrieve
+              their cherished resources from any corner of the world, on any device.
+              Effortless categorization ensures quick retrieval, enabling them to
+              stay focused and organized amidst their globetrotting endeavors.
+              Bookies have truly become an essential companion for those shaping
+              their professional journey from various horizons.</h3>
+          </div>
+        </div>
       </div>
-      <div class="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3" id="minbtn">
-      </div>
-      <div class="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3" id="maxbtn">
-      </div>
-      <div class="mx-auto pr-16" id="terminaltitle">
-        <p class="text-center text-sm">Terminal</p>
-      </div>
-
-    </div>
-    <div class="pl-1 pt-1 h-auto  text-green-200 font-mono text-xs bg-black " id="console">
-      <p class="pb-1">How it helps to boost our Productivity:</p>
-      <h3 class="pb-1">In the realm of productivity, Bookies have emerged as a guiding light
-          for digital nomads. Regardless of the diverse locations they traverse,
-          Bookies empower these modern wanderers to seamlessly manage their
-          bookmarks. With universal accessibility, digital nomads can retrieve
-          their cherished resources from any corner of the world, on any device.
-          Effortless categorization ensures quick retrieval, enabling them to
-          stay focused and organized amidst their globetrotting endeavors.
-          Bookies have truly become an essential companion for those shaping
-          their professional journey from various horizons.</h3>
-    </div>
-  </div> 
-</div>
-        </>
-    )
+    </>
+  )
 }
 export default Terminal
